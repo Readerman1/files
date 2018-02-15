f681b7a268df6e6d46a308757411b2f3e2e09dd4
Readerman1 <Delanofred@live.com>
2018-02-15 15:27:13 -0400

    Lab3

---
+++ text.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+	ofstream myfile {"abc.txt"};
+	if(myfile){
+			myfile <<a <<" "<<b<<" "<<c;
+		}else{
+			cout <<"error opening file abc.txt";
+		}
+		myfile.close();
+   cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+
+   cout<<endl<<endl;
+ 
+   return 0;
+}
